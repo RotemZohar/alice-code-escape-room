a82a487b2ed9e6f19bbee0f53b4be559743bf67a
Shira Solomon <shiruzruzon@gmail.com>
2022-05-12 19:28:26 +0300

    tried to add ursula

--- thelittlemermaid/thelittlemermaid.js
+++ thelittlemermaid/thelittlemermaid.js
@@ -5,10 +5,14 @@ function found(){
     document.getElementById('friend1').style.left="200px";
 }
 
-function found2(){
+function found2(src, width){
     // var img = document.createElement("img");
     // img.src = "";
     alert("you found me!");
     document.getElementById('friend2').style.left="200px";
     document.body.style.backgroundImage = "url('../Images/thelittlemermaid_background.PNG')";
+    var img = document.createElement("img");
+    img.src = "url('../Images/the_little_mermaid_ursula.PNG')";
+    img.width = '50%';
+    document.body.appendChild(img)
 }
