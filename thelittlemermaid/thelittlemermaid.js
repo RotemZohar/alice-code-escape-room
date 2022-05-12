8e9121568d17a9b573bcd4c56f309373ee021891
Shira Solomon <shiruzruzon@gmail.com>
2022-05-12 18:51:52 +0300

    added images

--- thelittlemermaid/thelittlemermaid.js
+++ thelittlemermaid/thelittlemermaid.js
@@ -2,4 +2,12 @@ function found(){
     // var img = document.createElement("img");
     // img.src = "";
     alert("you found me!");
+    document.getElementById('friend1').style.left="200px";
+}
+
+function found2(){
+    // var img = document.createElement("img");
+    // img.src = "";
+    alert("you found me!");
+    document.getElementById('friend2').style.left="200px";
 }
