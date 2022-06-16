c854ca070eb2a9f6d73a0c5a576e6b80ab04b1b5
IfatNeumann <ineumann19@gmail.com>
2022-06-16 14:30:39 +0100

    lion king - final touches

--- the_lion_king/lion_king.js
+++ the_lion_king/lion_king.js
@@ -2,22 +2,20 @@ imgCharacter1=false
 img1Character2=false
 img2Character3=false
 img3Character4=false
-imgCharacter=0
-//Character.delay(2000).fadeOut(3000); 
+
 var interval0 = appearAndDisappear("Character0",2000); 
 var interval1 = appearAndDisappear("Character1",1500); 
 var interval2 = appearAndDisappear("Character2",2500);
 var interval3 = appearAndDisappear("Character3",3000); 
 
-//צריך להוסיף קריאה לפונקצית תסט בכל אחת מהפונקציות של השחקנים,לשאול את רותם
-//לבדוק איך מעלימים את הדמויות לגמרי, להבין מה יעפת העלתה
-function test(){
-    if(imgCharacter == 4)
-    setTimeout(function() {
-        alert("Good job, you defeated the hyenas, and now the cave opening is clear, and we can go through it to the next room.")
-        alert("To move to the next room, click the cave opening")
-        nextroom()
-    }, 500);
+function checkIfDone(){
+    if(imgCharacter1 && img1Character2 && img2Character3 && img3Character4){
+        setTimeout(function() {
+            alert("Good job, you defeated the hyenas, and now the cave opening is clear, and we can go through it to the next room.")
+            alert("To move to the next room, click the cave opening")
+        }, 500);
+        document.getElementById("caveEntrence").onclick = function() {caveOpen()};
+    }
 }
 
 function img(){
@@ -25,9 +23,8 @@ function img(){
     clearInterval(interval0);
     var image = document.getElementById("Character0");
     image.style.transform = "scaleX(-1) skewY(333deg) rotate(88deg)"
-    imgCharacter=true
-    imgCharacter =+ 1
-
+    imgCharacter1=true
+    checkIfDone();
 }
 
 function img1(){
@@ -36,7 +33,7 @@ function img1(){
     var image = document.getElementById("Character1");
     image.style.transform = "scaleX(-1) skewY(333deg) rotate(151deg)"
     img1Character2=true
-    imgCharacter =+ 1
+    checkIfDone();
 
 }
 
@@ -46,8 +43,7 @@ function img2(){
     var image = document.getElementById("Character2");
     image.style.transform = "scaleX(-1) skewY(333deg) rotate(200deg)"
     img2Character3=true
-    imgCharacter =+ 1
-
+    checkIfDone();
 }
 
 function img3(){
@@ -56,8 +52,7 @@ function img3(){
     var image = document.getElementById("Character3");
     image.style.transform = "scaleX(-1) skewY(333deg) rotate(279deg)"
     img3Character4=true
-    imgCharacter =+ 1
-
+    checkIfDone();
 }
 
 function appearAndDisappear(elementId, time){
@@ -67,4 +62,8 @@ function appearAndDisappear(elementId, time){
         document.getElementById(elementId).style.visibility= hidden ? "visible" : "hidden";
         hidden = !hidden;
     },time);
+}
+
+function caveOpen(){
+    window.location.replace("../final_room.html");
 }
